41aa52eacb7dc36277dfd47cf15fb31e6e1d443a
Fabio Pinheiro <fabio.pinheiro@ifc.edu.br>
2023-01-17 15:59:40 -0300

    projeto quase pronto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-alert('olá')
+    // const subtrair = document.querySelector("#subtrair");
+    // const somar = document.querySelector("#somar");    
+    const controles = document.querySelectorAll("[data-controle]");
+
+    controles.forEach(function(controle) {
+        controle.addEventListener("click", function(evento) {
+            manipularDados(evento.target.textContent,
+                evento.target.parentNode.querySelector("[data-contador]"));
+
+        });
+    });
+
+    function manipularDados(operacao, contPeca) {
+        if (operacao == "-") {
+            contPeca.value--;
+            //console.log("subtrair");
+        } else
+        if (operacao == "+") {
+            contPeca.value++;
+            //console.log("somar");
+        }
+    }
+
+
+
+    // somar.addEventListener("click", function() {
+    //     //braco.value = parseInt(braco.value) + 1;
+    //     braco.value++;
+    // });
+
+    // subtrair.addEventListener("click", function() {
+    //     if (braco.value > 0)
+    //         braco.value--;
+    // });
